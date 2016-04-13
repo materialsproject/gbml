b3897f4b16c41d0d5f87997587274010434886f0
Shreyas Cholia <shreyas@gmail.com>
2016-04-13 14:57:21 -0700

    Add API key semantics to test_elasticity

--- gbml/examples/test_elasticity.py
+++ gbml/examples/test_elasticity.py
@@ -1,13 +1,15 @@
-#!/usr/local/bin/python2.7
+#!/usr/bin/env python
 
 # Test script for gbml elasticity (bulk and shear moduli) predictions
 
-import gbml.elasticity
+from gbml import elasticity
+
+elasticity.API_KEY = "YOUR_API_KEY_HERE"
 
 mpID = "mp-10003"
-(k_value, g_value, caveat_str) = gbml.elasticity.predict_k_g(mpID)
+(k_value, g_value, caveat_str) = elasticity.predict_k_g(mpID)
 print (k_value, g_value, caveat_str)
 
 mpID_list = ["mp-10003","mp-10010","mp-10015","mp-10021","mp-26","mp-10018","mp-19306"]
-(matid_list, k_list, g_list, caveat_list) = gbml.elasticity.predict_k_g_list(mpID_list)
+(matid_list, k_list, g_list, caveat_list) = elasticity.predict_k_g_list(mpID_list)
 print (matid_list, k_list, g_list, caveat_list)
